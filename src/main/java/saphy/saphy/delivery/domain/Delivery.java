86f8cafd8bb92011817f6c382364bee738f7c3d1
jae woo Jung <awfjol2008@gmail.com>
2024-07-15 19:01:20 +0900

    refactor: BaseEntity 디렉토리 위치 변경

--- src/main/java/saphy/saphy/delivery/domain/Delivery.java
+++ src/main/java/saphy/saphy/delivery/domain/Delivery.java
@@ -5,10 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import saphy.saphy.global.BaseEntity;
-import saphy.saphy.item.domain.Item;
+import saphy.saphy.global.entity.BaseEntity;
 import saphy.saphy.order.domain.Order;
-import saphy.saphy.user.domain.User;
 
 import java.time.LocalDateTime;
 
--- src/main/java/saphy/saphy/purchaseHistory/domain/PurchaseHistory.java
+++ src/main/java/saphy/saphy/purchaseHistory/domain/PurchaseHistory.java
@@ -5,8 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import saphy.saphy.global.BaseEntity;
-import saphy.saphy.item.domain.Item;
+import saphy.saphy.global.entity.BaseEntity;
 import saphy.saphy.order.domain.Order;
 import saphy.saphy.user.domain.User;
 
