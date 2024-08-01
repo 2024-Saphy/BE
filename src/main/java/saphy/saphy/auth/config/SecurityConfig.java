515b952bf7fa91f8d3a333977ccc9bb4d0e34e63
MinSang22Kim <powerminsang9901@gmail.com>
2024-08-01 19:11:45 +0900

    SAPHY-43 refactor: 카카오 로그인 수정

--- src/main/java/saphy/saphy/auth/config/SecurityConfig.java
+++ src/main/java/saphy/saphy/auth/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(
-                "/oauth/**"
+                "/oauth"
         );
     }
 
