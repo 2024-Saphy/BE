921a6602874f1ca213c6e620fcf7e3f9db71f6d7
MinSang22Kim <powerminsang9901@gmail.com>
2024-08-06 22:53:32 +0900

    SAPHY-41 refactor: 오타 수정 및 불필요한 코드 삭제

--- src/main/java/saphy/saphy/auth/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package saphy.saphy.auth.service;
-
-import saphy.saphy.auth.domain.dto.request.SocialLoginDTO;
-
-// 여러 소셜로그인의 확장성을 위해 인터페이스 구현
-public interface SecurityService {
-
-    void saveUserInSecurityContext(SocialLoginDTO socialLoginDTO);
-}
