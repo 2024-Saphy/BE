47bff0fad69c1c1d80d8302930d6bc74c51a799a
gyuseon <mn5592@naver.com>
2024-08-13 00:42:02 +0900

    SAPHY-54 feat : 회원 정보 변경 기능 구현

--- src/main/java/saphy/saphy/member/service/MemberService.java
+++ src/main/java/saphy/saphy/member/service/MemberService.java
@@ -11,6 +11,7 @@ import saphy.saphy.global.exception.ErrorCode;
 import saphy.saphy.global.exception.SaphyException;
 import saphy.saphy.member.domain.Member;
 import saphy.saphy.member.domain.dto.request.JoinMemberDto;
+import saphy.saphy.member.domain.dto.request.MemberInfoUpdateDto;
 import saphy.saphy.member.domain.dto.response.MemberInfoDto;
 import saphy.saphy.member.domain.repository.MemberRepository;
 import saphy.saphy.purchaseHistory.domain.PurchaseStatus;
@@ -19,6 +20,7 @@ import saphy.saphy.salesHistory.SalesStatus;
 import saphy.saphy.salesHistory.service.SalesHistoryService;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -50,7 +52,8 @@ public class MemberService {
     // 회원 정보 조회
     public MemberInfoDto getInfo() {
         String loginId = AccessTokenUtils.isPermission();
-        Member findMember = memberRepository.findByLoginId(loginId).orElse(null);
+        Member findMember = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> SaphyException.from(ErrorCode.MEMBER_NOT_FOUND));
 
         Map<DeliveryStatus, Long> deliveryCounts = deliveryService.getDeliveryStatusCounts(findMember.getId());
         Map<PurchaseStatus, Long> purchaseCounts = purchaseHistoryService.getPurchaseCounts(findMember.getId());
@@ -69,6 +72,29 @@ public class MemberService {
                 .salesInProgressCount(salesCounts.get(SalesStatus.IN_PROGRESS))
                 .salesCompletedCount(salesCounts.get(SalesStatus.COMPLETED))
                 .build();
+    }
+
+    // 회원 정보 수정
+    @Transactional
+    public void updateMemberInfo(MemberInfoUpdateDto updateDto) {
+        String loginId = AccessTokenUtils.isPermission();
+        Member findMember = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> SaphyException.from(ErrorCode.MEMBER_NOT_FOUND));
+
+
+        updateIfPresent(updateDto.getPassword(), findMember::setPassword);
+        updateIfPresent(updateDto.getName(), findMember::setName);
+        updateIfPresent(updateDto.getNickName(), findMember::setNickName);
+        updateIfPresent(updateDto.getAddress(), findMember::setAddress);
+        updateIfPresent(updateDto.getPhoneNumber(), findMember::setPhoneNumber);
+        updateIfPresent(updateDto.getEmail(), findMember::setEmail);
+
+        memberRepository.save(findMember);  // 변경된 내용을 저장합니다.
+    }
+
+    //주어진 값(value)이 null이 아닐 경우에만 특정 작업(setter)을 수행하도록 설계된 메서드, null 체크를 간단히 할 수 있음!
+    private <T> void updateIfPresent(T value, java.util.function.Consumer<T> setter) {
+        Optional.ofNullable(value).ifPresent(setter);
     }
 
 }
