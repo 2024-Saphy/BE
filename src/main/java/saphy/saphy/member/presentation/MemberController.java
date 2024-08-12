a81bcb2922a48df2ae62e3d6e75d086190b8f6e2
gyuseon <mn5592@naver.com>
2024-08-13 00:12:22 +0900

    SAPHY-53 feat : 회원 정보 조회 기능 구현

--- src/main/java/saphy/saphy/member/presentation/MemberController.java
+++ src/main/java/saphy/saphy/member/presentation/MemberController.java
@@ -3,15 +3,13 @@ package saphy.saphy.member.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import saphy.saphy.global.exception.ErrorCode;
 import saphy.saphy.global.exception.SaphyException;
 import saphy.saphy.global.response.ApiResponse;
 import saphy.saphy.member.domain.Member;
 import saphy.saphy.member.domain.dto.request.JoinMemberDto;
+import saphy.saphy.member.domain.dto.response.MemberInfoDto;
 import saphy.saphy.member.service.MemberService;
 
 @RestController
@@ -28,6 +26,12 @@ public class MemberController {
         validateRequest(errors);
         memberService.join(joinDto);
         return new ApiResponse<>(ErrorCode.REQUEST_OK);
+    }
+
+    @GetMapping("/info")
+    public ApiResponse<MemberInfoDto> getInfo() {
+        MemberInfoDto dto = memberService.getInfo();
+        return new ApiResponse<>(dto);
     }
 
     private void validateRequest(Errors errors) {
--- src/main/java/saphy/saphy/member/service/MemberService.java
+++ src/main/java/saphy/saphy/member/service/MemberService.java
@@ -4,11 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import saphy.saphy.auth.utils.AccessTokenUtils;
+import saphy.saphy.delivery.domain.DeliveryStatus;
+import saphy.saphy.delivery.service.DeliveryService;
 import saphy.saphy.global.exception.ErrorCode;
 import saphy.saphy.global.exception.SaphyException;
 import saphy.saphy.member.domain.Member;
 import saphy.saphy.member.domain.dto.request.JoinMemberDto;
+import saphy.saphy.member.domain.dto.response.MemberInfoDto;
 import saphy.saphy.member.domain.repository.MemberRepository;
+import saphy.saphy.purchaseHistory.domain.PurchaseStatus;
+import saphy.saphy.purchaseHistory.service.PurchaseHistoryService;
+import saphy.saphy.salesHistory.SalesStatus;
+import saphy.saphy.salesHistory.service.SalesHistoryService;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +26,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final DeliveryService deliveryService;
+    private final PurchaseHistoryService purchaseHistoryService;
+    private final SalesHistoryService salesHistoryService;
 
     // 회원 가입
     @Transactional
@@ -26,11 +39,36 @@ public class MemberService {
         memberRepository.save(joinMember);
     }
 
+
     private void validateExistMember(JoinMemberDto joinDto) {
         String loginId = joinDto.getLoginId();
         if (memberRepository.existsByLoginId(loginId)) {
             throw SaphyException.from(ErrorCode.DUPLICATE_MEMBER_LOGIN_ID);
         }
+    }
+
+    // 회원 정보 조회
+    public MemberInfoDto getInfo() {
+        String loginId = AccessTokenUtils.isPermission();
+        Member findMember = memberRepository.findByLoginId(loginId).orElse(null);
+
+        Map<DeliveryStatus, Long> deliveryCounts = deliveryService.getDeliveryStatusCounts(findMember.getId());
+        Map<PurchaseStatus, Long> purchaseCounts = purchaseHistoryService.getPurchaseCounts(findMember.getId());
+        Map<SalesStatus, Long> salesCounts = salesHistoryService.getPurchaseCounts(findMember.getId());
+
+        return MemberInfoDto.builder()
+                .nickname(findMember.getNickName())
+                //.profileImgUrl(findMember.getProfileImgUrl)
+                .deliveryStartedCount(deliveryCounts.get(DeliveryStatus.STARTED))
+                .deliveryGoingCount(deliveryCounts.get(DeliveryStatus.GOING))
+                .deliveryDeliveredCount(deliveryCounts.get(DeliveryStatus.DELIVERED))
+                .purchasePendingCount(purchaseCounts.get(PurchaseStatus.PENDING))
+                .purchaseInProgressCount(purchaseCounts.get(PurchaseStatus.IN_PROGRESS))
+                .purchaseCompletedCount(purchaseCounts.get(PurchaseStatus.COMPLETED))
+                .salesPendingCount(salesCounts.get(SalesStatus.PENDING))
+                .salesInProgressCount(salesCounts.get(SalesStatus.IN_PROGRESS))
+                .salesCompletedCount(salesCounts.get(SalesStatus.COMPLETED))
+                .build();
     }
 
 }
