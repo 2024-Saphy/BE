a0fb0eb855d858cf48526d8bafa15127f900bdfc
gyuseon <mn5592@naver.com>
2024-08-13 00:11:19 +0900

    SAPHY-53 feat : 구매 상태(구매 대기, 구매 중, 구매 완료)개수 얻어오는 기능 구현

--- src/main/java/saphy/saphy/purchaseHistory/domain/repository/PurchaseHistoryRepository.java
+++ src/main/java/saphy/saphy/purchaseHistory/domain/repository/PurchaseHistoryRepository.java
@@ -1,7 +1,14 @@
 package saphy.saphy.purchaseHistory.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import saphy.saphy.purchaseHistory.domain.PurchaseHistory;
+import saphy.saphy.purchaseHistory.domain.PurchaseStatus;
 
 public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory, Long> {
+
+    //구매 상태의 개수를 조회하는 메소드
+    @Query("SELECT COUNT(ph) FROM PurchaseHistory ph WHERE ph.purchaseStatus = :status AND ph.member.id = :memberId")
+    long countByPurchaseStatusAndMemberId(@Param("status") PurchaseStatus status, @Param("memberId") Long memberId);
 }
