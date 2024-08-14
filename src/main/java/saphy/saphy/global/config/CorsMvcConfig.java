87b49871928aa60485ce8a949faca2b221e498ea
MinSang22Kim <powerminsang9901@gmail.com>
2024-08-14 11:03:43 +0900

    SAPHY-43 refactor: config 파일 수정

--- src/main/java/saphy/saphy/global/config/CorsMvcConfig.java
+++ src/main/java/saphy/saphy/global/config/CorsMvcConfig.java
@@ -6,14 +6,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("https://saphy.site/","http://localhost:8080","http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true);
+        corsRegistry.addMapping("/**") // test용
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                // .allowCredentials(true) CORS 정책상 all 허용 불가
+                .allowedOriginPatterns("*")
+                .exposedHeaders("Authorization");
     }
+//    @Override
+//    public void addCorsMappings(CorsRegistry corsRegistry) {
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins("https://saphy.site/","http://localhost:8080","http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .exposedHeaders("Authorization")
+//                .allowCredentials(true);
+//    }
 }
