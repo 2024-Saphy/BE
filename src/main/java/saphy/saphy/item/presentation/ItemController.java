e689c8472e53225e1b838bc55e6c00fd3ea6ca3c
jae woo Jung <awfjol2008@gmail.com>
2024-08-30 21:57:14 +0900

    feat: ItemController 구현

--- src/main/java/saphy/saphy/item/presentation/ItemController.java
+++ src/main/java/saphy/saphy/item/presentation/ItemController.java
@@ -1,11 +1,38 @@
 package saphy.saphy.item.presentation;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import saphy.saphy.item.dto.request.ItemCreateRequest;
+import saphy.saphy.item.dto.response.ItemResponse;
+import saphy.saphy.item.service.ItemService;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api")
 public class ItemController {
+	private final ItemService itemService;
+
+	@PostMapping("/items")
+	public void save(@RequestBody ItemCreateRequest request) {
+		itemService.save(request);
+	}
+
+	@GetMapping("/items/{itemId}")
+	public ItemResponse findPhoneById(@PathVariable Long itemId) {
+		return itemService.findById(itemId);
+	}
+
+	@GetMapping("/items")
+	public List<ItemResponse> findAll() {
+		return itemService.findAll();
+	}
 }
