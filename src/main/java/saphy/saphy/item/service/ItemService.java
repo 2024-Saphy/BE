a84d606640790bbe96a526c48bc3eb1777c36b29
jae woo Jung <awfjol2008@gmail.com>
2024-08-30 21:57:04 +0900

    feat: ItemService 구현

--- src/main/java/saphy/saphy/item/service/ItemService.java
+++ src/main/java/saphy/saphy/item/service/ItemService.java
@@ -1,11 +1,36 @@
 package saphy.saphy.item.service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import saphy.saphy.item.domain.Item;
+import saphy.saphy.item.dto.request.ItemCreateRequest;
+import saphy.saphy.item.dto.response.ItemResponse;
+import saphy.saphy.item.repository.ItemRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ItemService {
+	private final ItemRepository itemRepository;
+
+	public void save(ItemCreateRequest request) {
+		Item phone = request.toEntity();
+		itemRepository.save(phone);
+	}
+
+	public ItemResponse findById(Long itemId) {
+		return itemRepository.findById(itemId)
+			.map(ItemResponse::from)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 휴대폰입니다."));
+	}
+
+	public List<ItemResponse> findAll() {
+		return itemRepository.findAll().stream()
+			.map(ItemResponse::from)
+			.toList();
+	}
 }
