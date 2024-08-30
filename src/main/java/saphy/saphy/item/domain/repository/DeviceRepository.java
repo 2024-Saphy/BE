f467a1fa870ac56522f51c1bc37d845c2dcc33fd
jae woo Jung <awfjol2008@gmail.com>
2024-08-30 21:55:13 +0900

    feat: 쓸 데 없는 클래스 제거

--- src/main/java/saphy/saphy/item/domain/repository/DeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package saphy.saphy.item.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import saphy.saphy.item.domain.Device;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-}
