61ca8fbf8e05acae24ab9e14901ebe7f0ca2e62e
jae woo Jung <awfjol2008@gmail.com>
2024-09-04 17:48:23 +0900

    feat: Phone 엔티티 구현

--- src/main/java/saphy/saphy/item/domain/Phone.java
+++ src/main/java/saphy/saphy/item/domain/Phone.java
@@ -1,52 +1,55 @@
-// package saphy.saphy.item.domain;
-//
-// import jakarta.persistence.Column;
-// import jakarta.persistence.DiscriminatorValue;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.EnumType;
-// import jakarta.persistence.Enumerated;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
-// import lombok.AccessLevel;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.experimental.SuperBuilder;
-// import saphy.saphy.global.entity.BaseEntity;
-// import saphy.saphy.item.domain.enumeration.Color;
-// import saphy.saphy.item.domain.enumeration.Storage;
-//
-// @Entity
-// @Getter
-// @SuperBuilder
-// @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// @DiscriminatorValue("PHONE")
-// @Table(name = "phones")
-// public class Phone extends Item {
-//
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name = "phone_id")
-//     private Long id;
-//
-//     @Column(name = "name", nullable = false)
-//     private String name;
-//
-//     @Column(name = "brand", nullable = false)
-//     private String brand;
-//
-//     @Column(name = "screen_size", nullable = false)
-//     private String screenSize;
-//
-//     @Enumerated(EnumType.STRING)
-//     @Column(name = "color", nullable = false)
-//     private Color color;
-//
-//     @Enumerated(EnumType.STRING)
-//     @Column(name = "storage", nullable = false)
-//     private Storage storage;
-//
-//     @Column(name = "grade", nullable = false)
-//     private String grade;
-// }
+package saphy.saphy.item.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import saphy.saphy.item.domain.enumeration.Brand;
+import saphy.saphy.item.domain.enumeration.Color;
+import saphy.saphy.item.domain.enumeration.Grade;
+import saphy.saphy.item.domain.enumeration.Storage;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("PHONE")
+@Table(name = "phones")
+public class Phone extends Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private Long id;
+
+    @Column(insertable = false, updatable = false)
+    private String deviceType;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "brand", nullable = false)
+    private Brand brand;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "color", nullable = false)
+    private Color color;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "storage", nullable = false)
+    private Storage storage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "grade", nullable = false)
+    private Grade grade;
+}
