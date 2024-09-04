4d2b3664b80f30858ba705b579500458eacb06a7
jae woo Jung <awfjol2008@gmail.com>
2024-09-04 17:48:13 +0900

    feat: Item 엔티티에 상속 관련 어노테이션 적용

--- src/main/java/saphy/saphy/item/domain/Item.java
+++ src/main/java/saphy/saphy/item/domain/Item.java
@@ -3,30 +3,27 @@ package saphy.saphy.item.domain;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import saphy.saphy.global.entity.BaseEntity;
-import saphy.saphy.item.domain.enumeration.Brand;
-import saphy.saphy.item.domain.enumeration.DeviceType;
-import saphy.saphy.item.domain.enumeration.Color;
-import saphy.saphy.item.domain.enumeration.Grade;
-import saphy.saphy.item.domain.enumeration.Storage;
 
 @Entity
 @Getter
-@Setter // Setter 어노테이션 없애고 로직 수정해야 함!!!
 @SuperBuilder
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "device_type")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "items")
 public class Item extends BaseEntity {
@@ -36,31 +33,12 @@ public class Item extends BaseEntity {
     @Column(name = "item_id")
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private DeviceType deviceType;
-
     @Column(name = "name", nullable = false)
     private String name;
 
     @Lob
     @Column(name = "description", nullable = false)
     private String description;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "brand", nullable = false)
-    private Brand brand;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "color", nullable = false)
-    private Color color;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "storage", nullable = false)
-    private Storage storage;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "grade", nullable = false)
-    private Grade grade;
 
     @Column(name = "price", nullable = false)
     private BigDecimal price;
