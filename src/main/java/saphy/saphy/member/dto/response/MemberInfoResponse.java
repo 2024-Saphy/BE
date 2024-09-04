6a77a771ead3adaf7e7869d1f96e4d7ca6d376ff
gyuseon <mn5592@naver.com>
2024-09-04 22:01:35 +0900

    SAPHY-53 refactor : 회원정보조회 기능 리팩토링

--- src/main/java/saphy/saphy/member/dto/response/MemberInfoResponse.java
+++ src/main/java/saphy/saphy/member/dto/response/MemberInfoResponse.java
@@ -2,6 +2,11 @@ package saphy.saphy.member.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import saphy.saphy.member.domain.Member;
+import saphy.saphy.purchase.domain.PurchaseStatus;
+import saphy.saphy.sales.SalesStatus;
+
+import java.util.Map;
 
 @Getter
 @Builder
@@ -21,4 +26,20 @@ public class MemberInfoResponse {
     private Long salesPendingCount;
     private Long salesInProgressCount;
     private Long salesCompletedCount;
+
+    public static MemberInfoResponse toDto(Member member, Map<PurchaseStatus, Long> purchaseCounts, Map<SalesStatus, Long> salesCounts) {
+                return MemberInfoResponse.builder()
+                        .nickname(member.getNickName())
+                        //.profileImgUrl(findMember.getProfileImgUrl)
+                        .deliveryStartedCount(purchaseCounts.get(PurchaseStatus.START))
+                        .deliveryGoingCount(purchaseCounts.get(PurchaseStatus.SHIPPED))
+                        .deliveryDeliveredCount(purchaseCounts.get(PurchaseStatus.DELIVERED))
+                        .purchasePendingCount(purchaseCounts.get(PurchaseStatus.PENDING))
+                        .purchaseInProgressCount(purchaseCounts.get(PurchaseStatus.PROCESSING))
+                        .purchaseCompletedCount(purchaseCounts.get(PurchaseStatus.DELIVERED))
+                        .salesPendingCount(salesCounts.get(SalesStatus.PENDING))
+                        .salesInProgressCount(salesCounts.get(SalesStatus.IN_PROGRESS))
+                        .salesCompletedCount(salesCounts.get(SalesStatus.COMPLETED))
+                        .build();
+    }
 }
