e60dcc376cda0703c3030eaeb474ed5d9d766c68
jae woo Jung <awfjol2008@gmail.com>
2024-09-06 00:33:20 +0900

    feat: 휴대폰 외에 태블릿 및 노트북 생성, 조회 기능 구현

--- src/main/java/saphy/saphy/item/service/ItemService.java
+++ src/main/java/saphy/saphy/item/service/ItemService.java
@@ -7,9 +7,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import saphy.saphy.item.domain.Item;
+import saphy.saphy.item.domain.Laptop;
 import saphy.saphy.item.domain.Phone;
+import saphy.saphy.item.domain.Tablet;
+import saphy.saphy.item.dto.request.LaptopCreateRequest;
 import saphy.saphy.item.dto.request.PhoneCreateRequest;
+import saphy.saphy.item.dto.request.TabletCreateRequest;
+import saphy.saphy.item.dto.response.LaptopResponse;
 import saphy.saphy.item.dto.response.PhoneResponse;
+import saphy.saphy.item.dto.response.TabletResponse;
 import saphy.saphy.item.repository.ItemRepository;
 
 @Service
@@ -18,22 +24,63 @@ import saphy.saphy.item.repository.ItemRepository;
 public class ItemService {
 	private final ItemRepository<Item> itemRepository;
 
-	public void save(PhoneCreateRequest request) {
-		Item phone = request.toEntity();
+	/**
+	 * 휴대폰
+	 */
+	public void savePhone(PhoneCreateRequest request) {
+		Phone phone = request.toEntity();
 		itemRepository.save(phone);
 	}
 
 	public PhoneResponse findPhoneById(Long itemId) {
 		return itemRepository.findById(itemId)
-			.map(Phone.class::cast)
 			.map(PhoneResponse::from)
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 휴대폰입니다."));
 	}
 
 	public List<PhoneResponse> findAllPhones() {
 		return itemRepository.findAll().stream()
-			.map(Phone.class::cast)
 			.map(PhoneResponse::from)
+			.toList();
+	}
+
+	/**
+	 * 태블릿
+	 */
+	public void saveTablet(TabletCreateRequest request) {
+		Tablet tablet = request.toEntity();
+		itemRepository.save(tablet);
+	}
+
+	public TabletResponse findTabletById(Long itemId) {
+		return itemRepository.findById(itemId)
+			.map(TabletResponse::from)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 태블릿입니다."));
+	}
+
+	public List<TabletResponse> findAllTablets() {
+		return itemRepository.findAll().stream()
+			.map(TabletResponse::from)
+			.toList();
+	}
+
+	/**
+	 * 노트북
+	 */
+	public void saveLaptop(LaptopCreateRequest request) {
+		Laptop laptop = request.toEntity();
+		itemRepository.save(laptop);
+	}
+
+	public LaptopResponse findLaptopById(Long itemId) {
+		return itemRepository.findById(itemId)
+			.map(LaptopResponse::from)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 노트북입니다."));
+	}
+
+	public List<LaptopResponse> findAllLaptops() {
+		return itemRepository.findAll().stream()
+			.map(LaptopResponse::from)
 			.toList();
 	}
 }
