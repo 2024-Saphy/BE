25245eec643e19f12b9bed46669fa0f062cb2f47
gyuseon <mn5592@naver.com>
2024-09-07 20:03:03 +0900

    SAPHY-66 feat : 찜 목록 삭제 기능 구현

--- src/main/java/saphy/saphy/itemWish/domain/repository/ItemWishRepository.java
+++ src/main/java/saphy/saphy/itemWish/domain/repository/ItemWishRepository.java
@@ -1,8 +1,10 @@
 package saphy.saphy.itemWish.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import saphy.saphy.item.domain.Item;
 import saphy.saphy.itemWish.domain.ItemWish;
+import saphy.saphy.member.domain.Member;
 
 public interface ItemWishRepository extends JpaRepository<ItemWish, Long> {
-
+    void deleteByMemberAndItem(Member member, Item item);
 }
