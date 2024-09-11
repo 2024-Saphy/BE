a7856f9fa48b63caf360943065eeb9522a7bc1e5
jae woo Jung <awfjol2008@gmail.com>
2024-09-11 22:01:07 +0900

    feat: 에러코드 추가 및 커스텀 Exception 생성자 추가

--- src/main/java/saphy/saphy/global/exception/SaphyException.java
+++ src/main/java/saphy/saphy/global/exception/SaphyException.java
@@ -5,13 +5,24 @@ import lombok.Getter;
 @Getter
 public class SaphyException extends RuntimeException {
 	private final ErrorCode errorCode;
+	private String message;
 
 	private SaphyException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
 	}
 
+	private SaphyException(ErrorCode errorCode,String message) {
+		super(message);
+		this.errorCode = errorCode;
+		this.message = message;
+	}
+
 	public static SaphyException from(ErrorCode errorCode) {
 		return new SaphyException(errorCode);
+	}
+
+	public static SaphyException from(ErrorCode errorCode,String message) {
+		return new SaphyException(errorCode,message);
 	}
 }
