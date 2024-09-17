8b47f57f8e8611e8f3bf49becccb15424dbc833b
jae woo Jung <awfjol2008@gmail.com>
2024-09-17 17:01:18 +0900

    feat: 전자기기 관련 Response DTO 관련 부모 클래스 구현

---
+++ src/main/java/saphy/saphy/item/dto/response/ItemResponse.java
@@ -0,0 +1,26 @@
+package saphy.saphy.item.dto.response;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import saphy.saphy.image.dto.response.ImageResponse;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // ItemResponse는 상속을 위한 클래스로, 그 자체로 사용되지 않기 떄문에 생성자를 private로 설정한다.
+public class ItemResponse {
+	protected Long id;
+
+	protected String deviceType;
+
+	protected String name;
+
+	protected String description;
+
+	protected int price;
+
+	protected int stock;
+
+	protected List<ImageResponse> images;
+}
