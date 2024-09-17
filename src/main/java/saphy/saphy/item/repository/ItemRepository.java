f2edd543e25600b818883c5997cc966219b96ce9
jae woo Jung <awfjol2008@gmail.com>
2024-09-18 00:06:43 +0900

    feat: 기기 종류에 따라 상품을 조회하는 메서드 구현

--- src/main/java/saphy/saphy/item/repository/ItemRepository.java
+++ src/main/java/saphy/saphy/item/repository/ItemRepository.java
@@ -1,8 +1,13 @@
 package saphy.saphy.item.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import saphy.saphy.item.domain.Item;
+import saphy.saphy.item.domain.enumeration.DeviceType;
 
 public interface ItemRepository<T extends Item> extends JpaRepository<T, Long> {
+	List<T> findByDeviceType(DeviceType deviceType);
+
 }
