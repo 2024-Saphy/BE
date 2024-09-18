c5bf5912a6c51868aaf5a102767ee770a0c8f4c6
jae woo Jung <awfjol2008@gmail.com>
2024-09-18 19:03:27 +0900

    feat: DeviceType Enum에 Response DTO 변환 로직 적용

--- src/main/java/saphy/saphy/item/domain/enumeration/DeviceType.java
+++ src/main/java/saphy/saphy/item/domain/enumeration/DeviceType.java
@@ -1,18 +1,30 @@
 package saphy.saphy.item.domain.enumeration;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import saphy.saphy.item.domain.Item;
+import saphy.saphy.item.dto.response.ItemResponse;
+import saphy.saphy.item.dto.response.LaptopResponse;
+import saphy.saphy.item.dto.response.PhoneResponse;
+import saphy.saphy.item.dto.response.TabletResponse;
 
 @Getter
 @AllArgsConstructor
 public enum DeviceType {
-	PHONE("휴대폰"),
-	TABLET("태블릿"),
-	LAPTOP("노트북");
+	PHONE("휴대폰", PhoneResponse::from),
+	TABLET("태블릿", TabletResponse::from),
+	LAPTOP("노트북", LaptopResponse::from);
 
 	private final String name;
+	private final Function<Item, ItemResponse> responseMapper;
+
+	// 람다를 사용해 response 생성
+	public ItemResponse mapResponse(Item item) {
+		return responseMapper.apply(item);
+	}
 
 	public static DeviceType findByName(String name) {
 		return Arrays.stream(DeviceType.values())
