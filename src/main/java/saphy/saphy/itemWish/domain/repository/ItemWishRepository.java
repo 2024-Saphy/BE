93fe36086373f9d04d5be918c4c232dc28702b9f
gyuseon <mn5592@naver.com>
2024-09-22 17:20:05 +0900

    SAPHY-65 feat : 찜 목록 조회 기능 구현

--- src/main/java/saphy/saphy/itemWish/domain/repository/ItemWishRepository.java
+++ src/main/java/saphy/saphy/itemWish/domain/repository/ItemWishRepository.java
@@ -1,10 +1,17 @@
 package saphy.saphy.itemWish.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import saphy.saphy.item.domain.Item;
 import saphy.saphy.itemWish.domain.ItemWish;
 import saphy.saphy.member.domain.Member;
 
+import java.util.List;
+
 public interface ItemWishRepository extends JpaRepository<ItemWish, Long> {
     void deleteByMemberAndItem(Member member, Item item);
+
+    @Query("SELECT iw.item FROM ItemWish iw WHERE iw.member = :member")
+    List<Item> findItemsByMember(@Param("member") Member member);
 }
