e4ba41b51f5046e946fa91befa6f4e1593add31f
jae woo Jung <awfjol2008@gmail.com>
2024-10-02 19:30:46 +0900

    feat: 상품 필터링 검색 및 정렬 기능 구현

--- src/main/java/saphy/saphy/item/service/ItemService.java
+++ src/main/java/saphy/saphy/item/service/ItemService.java
@@ -1,8 +1,7 @@
 package saphy.saphy.item.service;
 
-import java.util.List;
-
-import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +13,12 @@ import saphy.saphy.item.domain.Laptop;
 import saphy.saphy.item.domain.Phone;
 import saphy.saphy.item.domain.Tablet;
 import saphy.saphy.item.domain.enumeration.DeviceType;
+import saphy.saphy.item.domain.repository.ItemRepository;
 import saphy.saphy.item.dto.request.LaptopCreateRequest;
 import saphy.saphy.item.dto.request.PhoneCreateRequest;
+import saphy.saphy.item.dto.request.SearchParam;
 import saphy.saphy.item.dto.request.TabletCreateRequest;
 import saphy.saphy.item.dto.response.ItemResponse;
-import saphy.saphy.item.dto.response.LaptopResponse;
-import saphy.saphy.item.dto.response.PhoneResponse;
-import saphy.saphy.item.dto.response.TabletResponse;
-import saphy.saphy.item.domain.repository.ItemRepository;
 import saphy.saphy.member.domain.Member;
 
 @Service
@@ -31,33 +28,40 @@ public class ItemService {
 	private final ItemRepository<Item> itemRepository;
 
 	/**
-	 * 휴대폰
+	 * 공통
 	 */
-	public Item savePhone(PhoneCreateRequest request) {
-		Phone phone = request.toEntity();
-
-		return itemRepository.save(phone);
-	}
-
 	// deviceType 에 따라 각기 다른 응답을 반환하지만, 최종 반환형은 ItemResponse 로 통일. 이후 다시 자식 클래스로 형변환하여 자세한 정보를 전달
-	public ItemResponse findItemById(Long itemId) {
+	public ItemResponse findById(Long itemId) {
 		Item item = itemRepository.findById(itemId)
 			.orElseThrow(() -> SaphyException.from(ErrorCode.ITEM_NOT_FOUND));
 
 		return item.getDeviceType().mapResponse(item);
 	}
 
-	public List<ItemResponse> findAllItems() {
-		return itemRepository.findAll().stream()
-			.map(ItemResponse::from)
-			.toList();
+	public Slice<ItemResponse> findMainPageItemsByDeviceType(SearchParam searchParam, Pageable pageable) {
+		return itemRepository.findByDeviceType(DeviceType.valueOf(searchParam.getDeviceType()), pageable)
+			.map(ItemResponse::from);
 	}
 
-	public List<ItemResponse> findByDeviceType(String deviceType) {
-		return itemRepository.findByDeviceType(DeviceType.valueOf(deviceType))
-			.stream()
-			.map(ItemResponse::from)
-			.toList();
+	public Slice<ItemResponse> searchByDeviceType(SearchParam searchParam, Pageable pageable) {
+		return itemRepository.findByDeviceTypeAndSortType(searchParam, pageable)
+			.map(ItemResponse::from);
+	}
+
+	public void delete(Member member, Long itemId) {
+		if (!member.getIsAdmin()) {
+			throw SaphyException.from(ErrorCode.MEMBER_NOT_ADMIN);
+		}
+		itemRepository.deleteById(itemId);
+	}
+
+	/**
+	 * 휴대폰
+	 */
+	public Item savePhone(PhoneCreateRequest request) {
+		Phone phone = request.toEntity();
+
+		return itemRepository.save(phone);
 	}
 
 	/**
@@ -69,18 +73,6 @@ public class ItemService {
 		return itemRepository.save(tablet);
 	}
 
-	public TabletResponse findTabletById(Long itemId) {
-		return itemRepository.findById(itemId)
-			.map(TabletResponse::from)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 태블릿입니다."));
-	}
-
-	public List<TabletResponse> findAllTablets() {
-		return itemRepository.findAll().stream()
-			.map(TabletResponse::from)
-			.toList();
-	}
-
 	/**
 	 * 노트북
 	 */
@@ -88,24 +80,5 @@ public class ItemService {
 		Laptop laptop = request.toEntity();
 
 		return itemRepository.save(laptop);
-	}
-
-	public LaptopResponse findLaptopById(Long itemId) {
-		return itemRepository.findById(itemId)
-			.map(LaptopResponse::from)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 노트북입니다."));
-	}
-
-	public List<LaptopResponse> findAllLaptops() {
-		return itemRepository.findAll().stream()
-			.map(LaptopResponse::from)
-			.toList();
-	}
-
-	public void delete(Member member, Long itemId) {
-		if (!member.getIsAdmin()) {
-			throw SaphyException.from(ErrorCode.MEMBER_NOT_ADMIN);
-		}
-		itemRepository.deleteById(itemId);
 	}
 }
