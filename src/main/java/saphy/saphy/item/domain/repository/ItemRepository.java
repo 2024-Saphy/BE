c1345e870e0f796f2551627ae49db8523759fd9e
jae woo Jung <awfjol2008@gmail.com>
2024-10-02 19:25:28 +0900

    feat: ItemRepository 구현

--- src/main/java/saphy/saphy/item/domain/repository/ItemRepository.java
+++ src/main/java/saphy/saphy/item/domain/repository/ItemRepository.java
@@ -1,13 +1,13 @@
 package saphy.saphy.item.domain.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import saphy.saphy.item.domain.Item;
 import saphy.saphy.item.domain.enumeration.DeviceType;
+import saphy.saphy.item.domain.repository.custom.ItemQueryDslRepository;
 
-public interface ItemRepository<T extends Item> extends JpaRepository<T, Long> {
-	List<T> findByDeviceType(DeviceType deviceType);
-
+public interface ItemRepository<T extends Item> extends JpaRepository<T, Long>, ItemQueryDslRepository {
+	Slice<T> findByDeviceType(DeviceType deviceType, Pageable pageable);
 }
