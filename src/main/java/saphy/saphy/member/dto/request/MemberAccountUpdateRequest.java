5617a5afee2c7349072c1316a4beb6d3ecac4590
jae woo Jung <awfjol2008@gmail.com>
2024-10-04 20:14:09 +0900

    feat: 사용자 계좌 수정 request DTO 구현

---
+++ src/main/java/saphy/saphy/member/dto/request/MemberAccountUpdateRequest.java
@@ -0,0 +1,13 @@
+package saphy.saphy.member.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class MemberAccountUpdateRequest {
+	@NotNull(message = "은행명은 필수 값입니다.")
+	private String bankName;
+
+	@NotNull(message = "계좌번호는 필수 값입니다.")
+	private String accountNumber;
+}
