fd4b1cac2f34d4684c25f363b6a249572d58fcc2
jae woo Jung <awfjol2008@gmail.com>
2024-10-04 20:11:28 +0900

    feat: Account 클래스 생성자 구현

--- src/main/java/saphy/saphy/member/domain/Account.java
+++ src/main/java/saphy/saphy/member/domain/Account.java
@@ -1,9 +1,23 @@
 package saphy.saphy.member.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class Account {
 	private String bankName;
 	private String accountNumber;
+
+	public static Account of(String bankName, String accountNumber) {
+		Account newAccount = new Account();
+
+		newAccount.bankName = bankName;
+		newAccount.accountNumber = accountNumber;
+
+		return newAccount;
+	}
 }
