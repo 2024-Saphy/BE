3bae38d5125178e91e6c110870a8bec579e2cfe4
jae woo Jung <awfjol2008@gmail.com>
2024-10-04 20:11:34 +0900

    feat: Address 클래스 생성자 구현

--- src/main/java/saphy/saphy/member/domain/Address.java
+++ src/main/java/saphy/saphy/member/domain/Address.java
@@ -1,9 +1,24 @@
 package saphy.saphy.member.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class Address {
 	private String address;
+
 	private String detailAddress;
+
+	public static Address of(String address, String detailAddress) {
+		Address newAddress = new Address();
+
+		newAddress.address = address;
+		newAddress.detailAddress = detailAddress;
+
+		return newAddress;
+	}
 }
