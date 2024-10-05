62fe7a454bdd31e6b6bd8745213713f35d31ef3e
jae woo Jung <awfjol2008@gmail.com>
2024-10-05 21:00:37 +0900

    feat: 회원 조회 Response DTO에 주소 및 계좌 정보 추가

--- src/main/java/saphy/saphy/member/dto/response/MemberDetailResponse.java
+++ src/main/java/saphy/saphy/member/dto/response/MemberDetailResponse.java
@@ -8,21 +8,29 @@ import saphy.saphy.member.domain.SocialType;
 @Getter
 @Builder
 public class MemberDetailResponse {
-    private String loginId;
-    private SocialType socialType;
-    private String name;
-    private String nickName;
-    private String phoneNumber;
-    private String email;
+	private String loginId;
+	private SocialType socialType;
+	private String name;
+	private String nickName;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String detailAddress;
+	private String bankName;
+	private String accountNumber;
 
-    public static MemberDetailResponse toDto(Member member) {
-        return MemberDetailResponse.builder()
-                .loginId(member.getLoginId())
-                .socialType(member.getSocialType())
-                .name(member.getName())
-                .nickName(member.getNickName())
-                .phoneNumber(member.getPhoneNumber())
-                .email(member.getEmail())
-                .build();
-    }
+	public static MemberDetailResponse toDto(Member member) {
+		return MemberDetailResponse.builder()
+			.loginId(member.getLoginId())
+			.socialType(member.getSocialType())
+			.name(member.getName())
+			.nickName(member.getNickName())
+			.phoneNumber(member.getPhoneNumber())
+			.email(member.getEmail())
+			.address(member.getAddress().getAddress())
+			.detailAddress(member.getAddress().getDetailAddress())
+			.bankName(member.getAccount().getBankName())
+			.accountNumber(member.getAccount().getAccountNumber())
+			.build();
+	}
 }
