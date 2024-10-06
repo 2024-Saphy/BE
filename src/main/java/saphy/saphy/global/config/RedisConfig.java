782df69fc3692e9ab2ff0c7a372968cc821cbcb1
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-06 16:17:45 +0900

    SAPHY-168 feat: JedisConnectionFactory 연결 풀 설정

--- src/main/java/saphy/saphy/global/config/RedisConfig.java
+++ src/main/java/saphy/saphy/global/config/RedisConfig.java
@@ -20,8 +20,7 @@ public class RedisConfig {
     @Value("${spring.jwt.redis.password}")
     private String password;
 
-    // Redis 서버 연결 설정을 관리하는 객체
-    @Bean
+    @Bean // Redis 서버 연결 설정을 관리 객체
     public RedisConnectionFactory redisConnectionFactory() {
         // Redis 객체 생성 후 Redis 서버의 호스트, 포트, 비밀번호를 설정
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
@@ -35,10 +34,10 @@ public class RedisConfig {
         // JedisConnectionFactory를 통한 Redis 연결 생성
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisConfig);
 
-        // JedisConnectionFactory 연결 풀 설정
-        jedisConnectionFactory.getPoolConfig().setMaxTotal(50);  // 최대 커넥션 수
-        jedisConnectionFactory.getPoolConfig().setMaxIdle(30);   // 최대 유휴 커넥션 수
-        jedisConnectionFactory.getPoolConfig().setMinIdle(10);   // 최소 유휴 커넥션 수
+        // 연결 풀 설정 (최대 커넥션, 최대 유휴 커넥션, 최소 유휴 커넥션)
+        jedisConnectionFactory.getPoolConfig().setMaxTotal(50);
+        jedisConnectionFactory.getPoolConfig().setMaxIdle(30);
+        jedisConnectionFactory.getPoolConfig().setMinIdle(10);
 
         return jedisConnectionFactory;
     }
@@ -46,7 +45,8 @@ public class RedisConfig {
     @Bean // RedisTemplate 빈 생성: Redis 서버와 상호작용하기 위한 템플릿 객체
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());  // Redis 연결 팩토리 설정
+        // Redis 연결 팩토리를 RedisTemplate에 설정
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         // 일반적인 key:value 구조의 데이터에 대한 시리얼라이저 설정 (key와 value를 문자열로 변환)
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -59,6 +59,6 @@ public class RedisConfig {
         // 기본 시리얼라이저 설정 (모든 경우에 적용)
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 
-        return redisTemplate;  // 설정된 RedisTemplate 반환
+        return redisTemplate;
     }
 }
