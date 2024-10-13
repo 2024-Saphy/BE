ccb7a59825f46c0afcf9d0aa060fa67d8a083075
jae woo Jung <awfjol2008@gmail.com>
2024-10-13 21:03:35 +0900

    feat: Fetch Join을 활용한 사용자 조회 쿼리 메서드 구현

--- src/main/java/saphy/saphy/member/domain/repository/MemberRepository.java
+++ src/main/java/saphy/saphy/member/domain/repository/MemberRepository.java
@@ -1,6 +1,9 @@
 package saphy.saphy.member.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import saphy.saphy.member.domain.Member;
 import saphy.saphy.member.domain.SocialType;
 
@@ -14,4 +17,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Object> findByEmailAndSocialType(String email, SocialType socialType);
 
     Optional<Member> findByLoginIdAndSocialType(String loginId, SocialType socialType);
+
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.profileImage WHERE m.id = :memberId")
+    Optional<Member> findWithImagesById(@Param("memberId") Long memberId);
 }
