729cfc0dfaf26c8f0b610c4023be497dd4d18bbf
jae woo Jung <awfjol2008@gmail.com>
2024-10-13 20:17:26 +0900

    feat: 상품 상세 이미지 연관관계 설정 변경

--- src/main/java/saphy/saphy/item/domain/Item.java
+++ src/main/java/saphy/saphy/item/domain/Item.java
@@ -10,11 +10,13 @@ import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -62,7 +64,8 @@ public class Item extends BaseEntity {
 	@OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
 	private List<ItemImage> images = new ArrayList<>();
 
-	@OneToOne(mappedBy = "item", cascade = CascadeType.ALL)
+	@OneToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	@JoinColumn(name = "item_description_image_id")
 	private ItemDescriptionImage itemDescriptionImage;
 
 	public boolean canOrder() {
