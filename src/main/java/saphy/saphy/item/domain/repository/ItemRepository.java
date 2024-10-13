01b318a7adf54f74ce8e8b6195f32adf6ec77ce2
jae woo Jung <awfjol2008@gmail.com>
2024-10-13 20:17:49 +0900

    feat: Fetch Join을 사용한 상품 조회 로직 구현

--- src/main/java/saphy/saphy/item/domain/repository/ItemRepository.java
+++ src/main/java/saphy/saphy/item/domain/repository/ItemRepository.java
@@ -1,8 +1,12 @@
 package saphy.saphy.item.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import saphy.saphy.item.domain.Item;
 import saphy.saphy.item.domain.enumeration.DeviceType;
@@ -10,4 +14,7 @@ import saphy.saphy.item.domain.repository.custom.ItemQueryDslRepository;
 
 public interface ItemRepository<T extends Item> extends JpaRepository<T, Long>, ItemQueryDslRepository {
 	Slice<T> findByDeviceType(DeviceType deviceType, Pageable pageable);
+
+	@Query("SELECT i FROM Item i LEFT JOIN FETCH i.itemDescriptionImage WHERE i.id = :itemId")
+	Optional<T> findWithImagesById(@Param("itemId") Long itemId);
 }
