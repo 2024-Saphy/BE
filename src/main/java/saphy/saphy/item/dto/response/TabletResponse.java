04bf98d0677fb21a5c8d9d9977b158b08cb16f41
jae woo Jung <awfjol2008@gmail.com>
2024-10-13 20:21:34 +0900

    feat: 상품 Response DTO에 상품 상세 이미지 구현

--- src/main/java/saphy/saphy/item/dto/response/TabletResponse.java
+++ src/main/java/saphy/saphy/item/dto/response/TabletResponse.java
@@ -3,6 +3,7 @@ package saphy.saphy.item.dto.response;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import saphy.saphy.image.domain.Image;
 import saphy.saphy.image.dto.response.ImageResponse;
 import saphy.saphy.item.domain.Item;
 import saphy.saphy.item.domain.Tablet;
@@ -17,6 +18,8 @@ public class TabletResponse extends ItemResponse{
 	private String storage;
 
 	private String grade;
+
+	private ImageResponse descriptionImage;
 
 	public static TabletResponse from(Item item) {
 		TabletResponse response = new TabletResponse();
@@ -35,6 +38,9 @@ public class TabletResponse extends ItemResponse{
 		response.images = item.getImages().stream()
 			.map(itemImage -> ImageResponse.from(itemImage.getImage()))
 			.toList();
+		response.descriptionImage = ImageResponse.from(
+			Image.of("1", "1", "https://www.frisbeekorea.com/webfile/webedit/202409/20240910174241_w32uchi6j2.jpg"));
+
 
 		return response;
 	}
