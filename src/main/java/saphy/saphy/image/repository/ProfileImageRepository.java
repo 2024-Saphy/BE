8ff54152e9f8ad2180ba54e2e25b8608fcd1cc90
jae woo Jung <awfjol2008@gmail.com>
2024-10-15 15:21:41 +0900

    feat: 프로필 이미지 저장소 구현

--- src/main/java/saphy/saphy/image/repository/ProfileImageRepository.java
+++ src/main/java/saphy/saphy/image/repository/ProfileImageRepository.java
@@ -3,16 +3,17 @@ package saphy.saphy.image.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import saphy.saphy.image.domain.ProfileImage;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
 	Optional<ProfileImage> findByImage_StoreName(String storeName);
 
-	Optional<ProfileImage> findByMemberId(Long memberId);
+	// Optional<ProfileImage> findByMemberId(Long memberId);
 
-	// @Query("SELECT p FROM ProfileImage p JOIN FETCH p.member WHERE p.member.id = :memberId")
-	// Optional<ProfileImage> findByMemberIdJQL(Long memberId);
+	@Query("SELECT p FROM ProfileImage p JOIN FETCH p.member WHERE p.member.id = :memberId")
+	Optional<ProfileImage> findByMemberId(Long memberId);
 /*
 	@EntityGraph(attributePaths = { "url" })
 	Optional<String> fetchProfileImage();
